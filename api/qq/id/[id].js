a0fa0440fb3731dff5371ae45805aedbbca10e99
Sinofine Lotusie <i@sinofine.me>
2023-08-26 06:44:16 +0800

    chore: make id/xxx return MIME application/ttml+xml

--- api/qq/id/[id].js
+++ api/qq/id/[id].js
@@ -1,4 +1,8 @@
 import { lyricGet } from "../_utils/lyricGet.js"
 export default async (req, res)=>{
-    res.status(200).json(await lyricGet(req.query["id"],req.query["format"]))
+    if(req.query["format"]==="ttml"){
+        res.setHeader("Content-Type", "application/ttml+xml")
+        res.status(200).send((await lyricGet(req.query["id"],"ttml")).lyric)
+    } 
+    else res.status(200).json(await lyricGet(req.query["id"],req.query["format"]))
 }
